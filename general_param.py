ffaae9267ffd407a3ab518560856ab280a2e98c6
Jonas Liechti <jonas.i.liechti@protonmail.ch>
2022-06-14 17:26:47 +0200

    ordering the data files

--- general_param.py
+++ general_param.py
@@ -1,7 +1,10 @@
 import os
 
 data_path = os.path.join('.', 'data')
+interim_data_path = os.path.join(data_path, 'interim')
+engineered_data_path = os.path.join(data_path, 'engineered')
 binding_seqs = os.path.join(data_path, 'peak_data.txt')
 control_seqs = os.path.join(data_path, 'shuffled_data.txt')
 data_header = ['genome', 'chrom', 'chrom_start', 'chrom_stop', 'name', 'strength', 'orientation', 'sequence']
-cleanded_data = os.path.join(data_path, 'cleaned.csv')
+
+cleanded_data = os.path.join(interim_data_path, 'cleaned.p')
